838b2b65d776b282b29bd9b6c7609717ed995d90
Priti Chimanpure <priti.chimanpure921@gmail.com>
2026-02-16 18:19:15 +0530

    Fixed dotenv production crash

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ if(process.env.NODE_ENV != "production")
         require('dotenv').config() ;
 }
 
-console.log(require("dotenv").config());
+// console.log(require("dotenv").config());
 // console.log(process.env);
 
 // Start your server
